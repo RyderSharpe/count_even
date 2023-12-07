3e747383151beb404d0c91f3ab7800077c0070d1
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2023-12-07 01:32:31 -0800

    count_even.py

--- count_even.py
+++ count_even.py
@@ -10,7 +10,20 @@ for count in range(1, target + 2, 2):
     #print(count)
 print(sum)
 
+# or
 
+# even_sum = 0
+# for number in range(2, target + 1, 2):
+#   even_sum += number
+# print(even_sum)
+
+# or
+
+# alternative_sum = 0
+# for number in range(1, target + 1):
+#   if number % 2 == 0:
+#     alternative_sum += number
+# print(alternative_sum)
 
 
 
